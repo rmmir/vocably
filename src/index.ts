66d37a3a246c9365416e3f3146224b54214a9dc8
radumiron12 <radumiron12@gmail.com>
2024-08-17 23:35:11 +0300

    --chore: added jwt & cookie setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import fastify, { FastifyRequest, FastifyReply } from 'fastify'
+import fjwt from '@fastify/jwt'
+import fCookie from '@fastify/cookie'
 
 import { userRoutes } from './features/users/routes'
 import { userSchemas } from './features/users/schema'
 
 const app = fastify({ logger: true })
-
-app.get('/ping', async (request: FastifyRequest, reply: FastifyReply) => {
-    return 'pong\n'
-})
 
 app.listen({ port: 8080 }, (err, address) => {
     if (err) {
@@ -17,9 +15,13 @@ app.listen({ port: 8080 }, (err, address) => {
     console.log(`Server listening at ${address}`)
 })
 
+// JWT & Cookies setup
+app.register(fjwt, { secret: process.env.JWT_SECRET || 'fallback' })
+app.addHook('preHandler', (request: FastifyRequest, reply: FastifyReply) => request.jwt = app.jwt)
+app.register(fCookie, { secret: process.env.COOKIE_SECRET || 'fallback', hook: 'preHandler'})
+
 // Routes
 app.register(userRoutes, { prefix: 'v1/users' })
-
 
 // Schemas
 for (let schema of [...userSchemas]) {
